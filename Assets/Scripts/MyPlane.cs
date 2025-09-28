1348e4317db8b3db70ae13623f08f9dc754eb605
Nicolas Leon <nicolasaleon05@gmail.com>
2025-09-28 12:12:14 -0300

    Complete MyPlane

--- Assets/Scripts/MyPlane.cs
+++ Assets/Scripts/MyPlane.cs
@@ -13,7 +13,7 @@ namespace CustomMath
         public Vector3 Normal
         {
             get { return normal; }
-            set { normal = value; }
+            set { normal = value.normalized; }
         }
 
         public float Distance
@@ -26,64 +26,74 @@ namespace CustomMath
         #region Constructors
         public MyPlane(Vector3 inNormal, Vector3 inPoint)
         {
-            throw new NotImplementedException();
+            normal = inNormal.normalized;
+            distance = -Vector3.Dot(normal, inPoint);
         }
 
         public MyPlane(Vector3 inNormal, float d)
         {
-            throw new NotImplementedException();
+            normal = inNormal.normalized;
+            distance = d;
         }
 
         public MyPlane(Vector3 a, Vector3 b, Vector3 c)
         {
-            throw new NotImplementedException();
+            normal = Vector3.Normalize(Vector3.Cross(b - a, c - a));
+            distance = -Vector3.Dot(normal, a);
         }
         #endregion
 
         #region Functions
         public void SetNormalAndPosition(Vector3 inNormal, Vector3 inPoint)
         {
-            throw new NotImplementedException();
+            normal = inNormal.normalized;
+            distance = -Vector3.Dot(normal, inPoint);
         }
 
         public void Set3Points(Vector3 a, Vector3 b, Vector3 c)
         {
-            throw new NotImplementedException();
+            normal = Vector3.Normalize(Vector3.Cross(b - a, c - a));
+            distance = -Vector3.Dot(normal, a);
         }
 
         public void Flip()
         {
-            throw new NotImplementedException();
+            normal = -normal;
+            distance = -distance;
         }
 
         public void Translate(Vector3 translation)
         {
-            throw new NotImplementedException();
+            distance -= Vector3.Dot(normal, translation);
         }
 
         public static MyPlane Translate(MyPlane plane, Vector3 translation)
         {
-            throw new NotImplementedException();
+            plane.Translate(translation);
+            return plane;
         }
 
         public Vector3 ClosestPointOnPlane(Vector3 point)
         {
-            throw new NotImplementedException();
+            float dist = GetDistanceToPoint(point);
+            return point - normal * dist;
         }
 
         public float GetDistanceToPoint(Vector3 point)
         {
-            throw new NotImplementedException();
+            return Vector3.Dot(normal, point) + distance;
         }
 
         public bool GetSide(Vector3 point)
         {
-            throw new NotImplementedException();
+            return GetDistanceToPoint(point) > 0f;
         }
 
         public bool SameSide(Vector3 inPt0, Vector3 inPt1)
         {
-            throw new NotImplementedException();
+            float d0 = GetDistanceToPoint(inPt0);
+            float d1 = GetDistanceToPoint(inPt1);
+            return (d0 > 0f && d1 > 0f) || (d0 <= 0f && d1 <= 0f);
         }
 
         public bool Raycast(Ray ray, out float enter)
