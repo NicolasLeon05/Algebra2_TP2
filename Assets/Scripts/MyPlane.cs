4f3fd9ba514337d68f5a63b107720ed59955f4c0
Nicolas Leon <nicolasaleon05@gmail.com>
2025-09-28 22:34:02 -0300

    Delete unused functions

--- Assets/Scripts/MyPlane.cs
+++ Assets/Scripts/MyPlane.cs
@@ -24,24 +24,9 @@ namespace CustomMath
             distance = -Vector3.Dot(normal, inPoint);
         }
 
-        public MyPlane(Vector3 inNormal, float d)
-        {
-            normal = inNormal.normalized;
-            distance = d;
-        }
-
         #endregion
 
         #region Functions
-        public void SetNormalAndPosition(Vector3 inNormal, Vector3 inPoint)
-        {
-            
-        }
-
-        public void Set3Points(Vector3 a, Vector3 b, Vector3 c)
-        {
-            
-        }
 
         public void Flip()
         {
@@ -54,18 +39,6 @@ namespace CustomMath
             distance -= Vector3.Dot(normal, translation);
         }
 
-        public static MyPlane Translate(MyPlane plane, Vector3 translation)
-        {
-            plane.Translate(translation);
-            return plane;
-        }
-
-        public Vector3 ClosestPointOnPlane(Vector3 point)
-        {
-            float dist = GetDistanceToPoint(point);
-            return point - normal * dist;
-        }
-
         public float GetDistanceToPoint(Vector3 point)
         {
             return Vector3.Dot(normal, point) + distance;
@@ -74,18 +47,6 @@ namespace CustomMath
         public bool GetSide(Vector3 point)
         {
             return GetDistanceToPoint(point) > 0f;
-        }
-
-        public bool SameSide(Vector3 inPt0, Vector3 inPt1)
-        {
-            float d0 = GetDistanceToPoint(inPt0);
-            float d1 = GetDistanceToPoint(inPt1);
-            return (d0 > 0f && d1 > 0f) || (d0 <= 0f && d1 <= 0f);
-        }
-
-        public bool Raycast(Ray ray, out float enter)
-        {
-            throw new NotImplementedException();
         }
         #endregion
 
